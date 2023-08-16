de8d6da33a083875e04c3a3b22bc0f68d2289fbf
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-08-16 13:16:22 -0500

    New comments and formatting

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,5 @@
-   // This demo shows off comments!
-
-   // console.log("This does not print.");
-
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
-
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+//Comments can be only one line long!     
+     
+     console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
    console.log("Comments make your code more readable by others.");
