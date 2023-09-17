a45a0b7828ca71036d8fcbaac04d390d23308fac
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-09-17 10:45:05 -0500

    With my solutions

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,36 @@
-let num = 1001;
+/*let num = 1001;
 
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+num = String(num);
+console.log(num.length);
+*/
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+num = 12345
+num = String(num);
+
+numDecimalLength = (num.length - 1);
+numDecimalLength = String(numDecimalLength);
+
+console.log(num.length);
+console.log(numDecimalLength);
+
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (num.indexOf(".") === -1) {
+    console.log(num.length);
+} else {
+    console.log(numDecimalLength);
+}
+
+/* Textbook Solution:
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
+
+    */
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,34 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1) + language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+
+let initials = language.charAt(0).concat("", language.charAt(4));
+console.log(initials);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+// console.log(`The abbreviation for '${language}' is '${language[0]}${language[4]}'.`);
+console.log(`The abbreviation for '${language}' is '${initials}'.`);
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+
+console.log(language.slice(4).charAt(2).toUpperCase());
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+//let Title = notTitleCase.charAt(0).toUpperCase().concat("", notTitleCase.slice(1, 6));
+// console.log(Title);
+//let Case = notTitleCase.charAt(6).toUpperCase().concat("", notTitleCase.slice(7));
+// console.log(Case);
+
+//console.log(Title.concat("", Case));
+
+//console.log(`${notTitleCase.charAt(0).toUpperCase()}${notTitleCase.slice(1, 6)}${notTitleCase.charAt(6).toUpperCase()}${notTitleCase.slice(7)}`);
+
+console.log(notTitleCase.replace("t", "T").replace("c", "C"));
