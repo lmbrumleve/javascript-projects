38b7ecb560e1767da410a2be2e9cd537842a32c4
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-09-18 23:07:24 -0500

    Studio 3 Complete

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -5,10 +5,56 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+foodArray = food.split(',').sort();
+equipmentArray = equipment.split(',').sort();
+petsArray = pets.split(',').sort();
+sleepAidsArray = sleepAids.split(',').sort();
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
 
+cargoHold = [foodArray, equipmentArray, petsArray, sleepAidsArray];
+
+console.log(cargoHold);
+
+
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+let userAnswer = input.question("Select a cabinet (0-3) in the cargoHold.");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
+/*
+if ((userAnswer < 0) || (userAnswer > 3) || (userAnswer.includes('.') === true) === true) {
+    console.log("ERROR: INVALID NUMBER");
+} else {
+    console.log(cargoHold[Number(userAnswer)]);
+}
+*/
+
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+// let particularItem = input.question("Enter a particular item from the cargo hold:")
+
+// if (((userAnswer < 0) || (userAnswer > 3) || (userAnswer.includes('.') === true) === false) 
+// && (cargoHold[Number(userAnswer)].includes(particularItem) === true)) {
+//     console.log(`Cabinet ${userAnswer} does include ${particularItem}`);
+// } else {
+//     console.log("ERROR: INVALID NUMBER");
+// }
+
+let item = ""
+
+if ((userAnswer < 0) || (userAnswer > 3) || (userAnswer.includes('.') === true) === true) {
+    console.log("ERROR: INVALID NUMBER");
+
+} else if ((userAnswer < 0) || (userAnswer > 3) || (userAnswer.includes('.') === true) === false) {
+    console.log(cargoHold[Number(userAnswer)]);
+    item = input.question("Enter a particular item from the cargo hold:");
+
+
+        if (cargoHold[Number(userAnswer)].includes(item) === true) {
+            console.log(`Cabinet ${userAnswer} does include ${item}.`);
+        } else {
+             console.log("ERROR");
+        }
+    }
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,35 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let str1 = str.slice(3);
+let str2 = str.slice(0,3);
+
+// console.log(str1);
+// console.log(str2);
+
+let strPigLatin3 = str1.concat(str2);
+
+console.log(strPigLatin3);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`Pig Latin for '${str}' is '${strPigLatin3}'.`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let userAnswer = input.question("How many letters will be relocated?");
+
+let strUserAnswer1 = str.slice(Number(userAnswer));
+let strUserAnswer2 = str.slice(0, Number(userAnswer));
+
+let strPigLatinUserAnswer = strUserAnswer1.concat(strUserAnswer2);
+// console.log(strPigLatinUserAnswer);
+
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if (Number(userAnswer) > 10) {
+    console.log(`Error. LaunchCode only has 10 characters. Pig Latin for '${str}' is '${strPigLatin3}'.`);
+} else {
+    console.log(`Pig Latin for '${str}' with ${userAnswer} relocated letters is '${strPigLatinUserAnswer}'.`);
+}
