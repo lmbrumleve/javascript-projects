b95d7f573361662cc3dd957a68e4011cdaba7390
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-11-06 22:01:35 -0600

    Fetch studio complete

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,25 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            for (let i=0; i<json.length; i++) {
+                let astronaut = json[i];
+
+            container.innerHTML += 
+
+        `<div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                <ul>
+                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                <li>Active: ${astronaut.active}</li>
+                <li>Skills: ${astronaut.skills.join(", ")}</li>
+                </ul>
+            </div>
+            <img class="avatar" src=${astronaut.picture}>
+        </div>`
+            }
+        })
+    })
+})
